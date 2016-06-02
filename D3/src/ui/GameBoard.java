4724625e9eaffe840bd56e3f952b5aba1d85a682
sushilApp <patilsushil1991@gmail.com>
2016-06-02 17:22:01 -0400

    Final change in music

--- D3/src/ui/GameBoard.java
+++ D3/src/ui/GameBoard.java
@@ -405,10 +405,10 @@ public class GameBoard extends GUIParent {
         lblPlayerMove.setVisible(false);
         invalidMove.setVisible(false);
         boardEnable = false;
-        if(!result.substring(0).contains("Computer"))
-        	JOptionPane.showMessageDialog(null, gameBoard.getPanel(result,"/winner.gif"),"Game Result",JOptionPane.INFORMATION_MESSAGE);
-        else 
-        	JOptionPane.showMessageDialog(null, gameBoard.getPanel(result,"") ,"Game Result",JOptionPane.INFORMATION_MESSAGE);
+//        if(!result.substring(0).contains("Computer"))
+//        	JOptionPane.showMessageDialog(null, gameBoard.getPanel(result,"/winner.gif"),"Game Result",JOptionPane.INFORMATION_MESSAGE);
+//        else 
+//        	JOptionPane.showMessageDialog(null, gameBoard.getPanel(result,"") ,"Game Result",JOptionPane.INFORMATION_MESSAGE);
 
         if(mode==1 && turn == "Computer")
         	JOptionPane.showMessageDialog(null, result ,"Game Result",JOptionPane.INFORMATION_MESSAGE);
