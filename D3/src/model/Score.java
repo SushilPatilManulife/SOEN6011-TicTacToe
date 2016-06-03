8d677ce1b413913ffa8186f548658e0fc98806aa
amirhakim <amirhakim@gmail.com>
2016-06-03 01:01:59 -0400

    Player History and top ten player feature added: -TODO: link it to the controller

--- D3/src/model/Score.java
+++ D3/src/model/Score.java
@@ -1,41 +1,62 @@
 package model;
 
-import javax.xml.bind.annotation.XmlAttribute;
+import java.util.HashMap;
+import java.util.Map;
+ 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 
-@XmlRootElement 
-public class Score {
+@XmlRootElement
+public class Score
+{ 
+  @XmlJavaTypeAdapter(MapAdapter.class)
+  public Map<String, Integer> standings;
+ 
+  public Score()
+  {
+    standings = new HashMap<String, Integer>();
+  }
+ 
+ 
+public void addResult(String name, Integer score)   { standings.put(name, score); }
 
-	String name;
-	int score;
-	int id;
-
-	public String getName() {
-		return name;
-	}
-
-	@XmlElement
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getAge() {
-		return score;
-	}
-
-	@XmlElement
-	public void setAge(int age) {
-		this.score = age;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	@XmlAttribute
-	public void setId(int id) {
-		this.id = id;
-	}
+}
+ 
+class MapAdapter extends XmlAdapter<MapElements[], Map<String, Integer>>
+{
+  public MapElements[] marshal(Map<String, Integer> arg0) throws Exception
+  {
+    MapElements[] mapElements = new MapElements[arg0.size()];
+ 
+    Integer i = 0;
+    for (Map.Entry<String, Integer> entry : arg0.entrySet())
+      mapElements[i++] = new MapElements(entry.getKey(), entry.getValue());
+ 
+    return mapElements;
+  }
+ 
+  public Map<String, Integer> unmarshal(MapElements[] arg0) throws Exception
+  {
+    Map<String,Integer> r = new HashMap<String,Integer>();
+    for(MapElements mapelement : arg0)
+      r.put(mapelement.key, mapelement.value);
+    return r;
+  }
+}
+ 
+class MapElements
+{
+  @XmlElement public String  key;
+  @XmlElement public Integer value;
+ 
+  private MapElements() {} //Required by JAXB
+ 
+  public MapElements(String key, Integer value)
+  {
+    this.key   = key;
+    this.value = value;
+  }
 }
--- D3/src/model/XmlIO.java
+++ D3/src/model/XmlIO.java
@@ -1,63 +1,114 @@
 package model;
 
 import java.io.File;
+import java.util.*;
+import java.util.Map.Entry;
 import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.Reader;
+import java.io.Writer;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamReader;
 
-public class XmlIO {
-
-	private String fileName = "score.xml";
+ 
+ 
+public class XmlIO
+{
+	private static final String fileName = "score.xml";
 	
-	public XmlIO() {
-		
-	}
+  private static void writeAsXml(Object o, Writer writer) throws Exception
+  {
+    JAXBContext jaxb = JAXBContext.newInstance(o.getClass());
+ 
+    Marshaller xmlConverter = jaxb.createMarshaller();
+    xmlConverter.setProperty("jaxb.formatted.output", true);
+    xmlConverter.marshal(o, writer);
+  }
+ 
+  private static <T> T readObjectAsXmlFrom(Reader reader, Class<T> c) throws Exception
+  {
+    JAXBContext jaxb = JAXBContext.newInstance(c);
+ 
+    XMLStreamReader xmlReader =
+      XMLInputFactory.newInstance().createXMLStreamReader(reader);
+ 
+    Unmarshaller xmlInterpreter = jaxb.createUnmarshaller();
+ 
+    return xmlInterpreter.unmarshal(xmlReader, c).getValue();
+  }
+  // Allow to insert any score or player
+  public static void addScore(String name, int score) throws Exception{
+	  File xmlFile = new File(fileName);
+	  Score root = new Score();
+    root = readObjectAsXmlFrom(new FileReader(xmlFile.getAbsolutePath()), root.getClass());
+    root.addResult(name,score);
+    writeAsXml(root, new FileWriter(xmlFile));
+  }
+  // increment wining record if player exist or create new entry if not exist 
+  public static void addWin(String name) throws Exception{
+	  Integer newScore = 1;
+	  Map<String, Integer> standings = new TreeMap<String, Integer>();
+	  File xmlFile = new File(fileName);
+	  Score root = new Score();
+    root = readObjectAsXmlFrom(new FileReader(xmlFile.getAbsolutePath()), root.getClass());
+    standings =  root.standings;
+    if(standings.containsKey(name)) {
+    	int current = standings.get(name);
+    	newScore = current + 1;
+    }
+    root.addResult(name,newScore);
+    writeAsXml(root, new FileWriter(xmlFile));
+  }
+  // top ten players on file
+  public static ArrayList<String> getTopTen() throws Exception{
 	
-	public Boolean saveXML(Score score) {
-		try {
-			File file = new File(fileName);
-			JAXBContext jaxbContext = JAXBContext.newInstance(score.getClass());
-			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+	int start, end;
+	File xmlFile = new File(fileName);
+	Score root = new Score();
+	Map<String, Integer> standings = new TreeMap<String, Integer>();
+    root = readObjectAsXmlFrom(new FileReader(xmlFile.getAbsolutePath()), root.getClass());
+    standings = root.standings;
+    Map<String, Integer> sortedStandings = sortByValue(standings);
+    Set<Entry<String, Integer>> keys = sortedStandings.entrySet();
+    Object[] sortedArr = keys.toArray();
+    end = sortedArr.length;
+    if(end<=10) start = 0;
+    else start = end -10;
+    ArrayList<String> result = new ArrayList<String>();
+    for(int x = start; x<end;x++){
+    	result.add(sortedArr[x].toString());
+    }
+	return result;
+  }
+  
+  public static <K, V extends Comparable<? super V>> Map<K, V> 
+  sortByValue( Map<K, V> map )
+{
+	  LinkedList<Map.Entry<K, V>> list = new LinkedList<Map.Entry<K, V>>( map.entrySet() );
+  Collections.sort( list, new Comparator<Map.Entry<K, V>>()
+  {
+      public int compare( Map.Entry<K, V> o1, Map.Entry<K, V> o2 )
+      {
+          return (o1.getValue()).compareTo( o2.getValue() );
+      }
+  } );
 
-			// output pretty printed
-			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+  Map<K, V> result = new LinkedHashMap<K, V>();
+  for (Map.Entry<K, V> entry : list)
+  {
+      result.put( entry.getKey(), entry.getValue() );
+  }
+  return result;
+}
 
-			jaxbMarshaller.marshal(score, file);
-			jaxbMarshaller.marshal(score, System.out);
-
-		      } catch (JAXBException e) {
-			e.printStackTrace();
-		      }
-		return true;
-	}
-	
-	public Score readXML() {
-		Score score = new Score();
-		try {
-
-			File file = new File(fileName);
-			JAXBContext jaxbContext = JAXBContext.newInstance(Score.class);
-
-			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-			score = (Score) jaxbUnmarshaller.unmarshal(file);
-			System.out.println(score);
-			return score;
-		  } catch (JAXBException e) {
-			e.printStackTrace();
-		  }
-		return score;
-	}
-	
-	public static void main(String[] args) {
-		XmlIO log = new XmlIO();
-		Score score = new Score();
-		score.setId(100);
-		score.setAge(29);
-		score.setName("Bob");
-		log.saveXML(score);
-		Score score2 =  new Score();
-		score2 = log.readXML();
-		System.out.println(score2.getAge());
-	}
+// for testing and demo 
+//  public static void main(String... args) throws Exception
+//  {
+////	  addScore("Mani", 350);
+//	  addWin("Mani");
+//	  System.out.println(getTopTen());
+//  }
 }
